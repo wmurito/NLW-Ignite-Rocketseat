d87f7048d5bc7a65688fa6e24d39d2a9c83d010a
wmurito <wellington_murito@hotmail.com>
2022-09-19 09:22:47 -0300

    firts commit

---
+++ esports/mobile/src/routes/app.routes.tsx
@@ -0,0 +1,22 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Home } from '../Screens/Home';
+import { Game } from '../Screens/Home/Game';
+
+const { Navigator, Screen} = createNativeStackNavigator();
+
+export function AppRoutes(){
+    return (
+        <Navigator screenOptions = {{headerShown: false}}>
+            <Screen
+                name= 'home'
+                component={Home}
+                
+            />
+            <Screen
+                name= 'game'
+                component={Game}
+            />
+        </Navigator>
+    )
+}
---
+++ esports/web/src/App.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react'
+import * as Dialog from '@radix-ui/react-dialog'
+
+
+import { GameBanner } from './components/GamerBanner'
+import { CreateAdBanner } from './components/CreatedAdBanner'
+import logoImg from './assets/Logo.svg';
+
+import './styles/main.css'
+import { CreateAdModal } from './components/CreatedAdModal'
+import axios from 'axios'
+
+interface Game {
+  id: string
+  title: string
+  bannerUrl: string
+  _count: {
+    ads: number
+  }
+}
+
+function App() {
+  const [games, setGames] = useState<Game[]>([])
+
+  useEffect(() => {
+    axios('http://localhost:3333/games').then(res => {
+      setGames(res.data)
+    })
+  }, [])
+
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="logo" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu
+        <span className="text-transparent bg-nlw-gradient bg-clip-text"> duo </span>
+        está aqui!
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        {games.map(game => {
+          return (
+            <GameBanner
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads} />
+          )
+        })}
+
+      </div>
+
+      <Dialog.Root>
+        <CreateAdBanner />
+
+        <CreateAdModal />
+
+      </Dialog.Root>
+
+    </div >
+  )
+}
+
+export default App
